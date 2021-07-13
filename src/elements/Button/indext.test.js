7601f464ddd0a2f7ab3d3ba07a199d0bcdb46ce8
Bee-habibi <Habibijrr11@gmail.com>
2021-07-13 21:39:42 +0700

    Mern HariMukti

---
+++ src/elements/Button/indext.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Button from "./index";
+
+test("Should not allowed click button if isDisabled is present", () => {
+  const { container } = render(<Button isDisabled></Button>);
+
+  expect(container.querySelector("span.disabled")).toBeInTheDocument();
+});
+
+test("Should render loading/spinner", () => {
+  const { container, getByText } = render(<Button isLoading></Button>);
+
+  expect(getByText(/loading/i)).toBeInTheDocument();
+  expect(container.querySelector("span")).toBeInTheDocument();
+});
